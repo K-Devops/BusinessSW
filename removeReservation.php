345a564889249454b6c132e46fb530cfa0742b06
alilibu <Februar2302!>
2021-12-22 18:53:47 +0100

    removeReservation

---
+++ removeReservation.php
@@ -0,0 +1,25 @@
+<?php
+$user = 'root';
+$pass = 'root';
+header('Content-Type: application/json');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=scooterhub', $user, $pass);
+// Read JSON Body of request
+$data = json_decode(file_get_contents('php://input'), true);
+$id = trim($data['id'] ?? '');
+if(strlen($id) == 0) {
+echo json_encode(["ok" => false, 'id' => $id]);
+} else {
+$statement = $pdo->prepare("Delete FROM reservation WHERE SCOOTER_ID = (?)");
+$ok = $statement->execute([$id]);
+echo json_encode(["ok" => $ok]);
+}
+?>
