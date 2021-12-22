c42d8e0f12de7c84bf385821c8c4a9661b776009
alilibu <Februar2302!>
2021-12-22 18:51:09 +0100

    getSCooterStatus

---
+++ getScooterStatus.php
@@ -0,0 +1,20 @@
+<?php
+$user = 'root';
+$pass = 'root';
+header('Content-Type: application/json');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=scooterhub', $user, $pass);
+$statement = $pdo->prepare("SELECT * FROM Scooter");
+// nur Status: $statement = $pdo->prepare("SELECT RESERVATION_STATUS FROM Scooter");
+$statement->execute();
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($data);
+?>
