b62cf8a8d826279735b416230ab1b672a2ff9e3e
alilibu <Februar2302!>
2021-12-22 18:55:40 +0100

    duplicates removed

--- removeReservation.php
+++ removeReservation.php
@@ -2,7 +2,6 @@
 $user = 'root';
 $pass = 'root';
 header('Content-Type: application/json');
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
